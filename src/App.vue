644a49da13ddab0fb6514ecd35810baf009ac5db
BrunoDehon <bruno.dehon@ynov.com>
2020-05-11 12:38:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+    <div id="nav">
+            <router-link to="/">DevJokes</router-link>
+
+    </div>
+    <div id="img">
+      <div id="opacity">
+  
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0%;
+  padding: 0%;
+  
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #FfF;
+  
+}
+
+#nav {
+  height: 70PX;
+  background-color: lightgrey;
+  Position:fixed;
+  z-index:8;
+  width: 100%;
+  height: 50px;
+  color: white;
+}
+
+#nav a {
+  font-weight: bold;
+  color: white;
+}
+
+img {
+  position: absolute;
+  top: 2.5px;
+  left: 5px;
+}
+
+#nav a.router-link-exact-active {
+  position:absolute;
+  color:  #000000;
+  top: 16px;
+  text-align:center;
+  text-decoration: none;
+
+}
+#img{
+  top:50px;
+  width: 100%;
+  height: 250px;
+  position:relative;
+  background-image: url("./assets/Banner.jpg");
+  
+}
+#opacity{
+  width: 100%;
+  height: 100%;
+  position:absolute;
+  z-index:4;
+  background-color: rgba(255, 255, 255,0.5);  
+  
+}
+</style>
---
+++ src/views/Joke.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="fond" style="padding-top: 50px">
+        <div v-for="element in results" :key="element.id" >
+            <br>
+            <br>
+            <span class="boldRed"></span><span class="text">{{ element.id }}</span>
+            <br>
+            <br>
+            <span class="boldRed">{{ element.setup }}</span>
+            <br>
+            <br>
+            <span class="boldRed">{{ element.punchline }}</span>
+            <br>
+            <br>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+    export default {
+        name: "Joke",
+        data(){
+            return{
+                results: []
+            }
+        },
+        methods: {
+            getMyJoke(){
+                axios.get('https://official-joke-api.appspot.com/jokes/programming/ten')
+                .then((joke) => {
+                    console.log(joke.request.response);
+                    this.results = JSON.parse(joke.request.response)
+                })
+                .catch(() => {
+                    this.results = "PAS DE JOKE JE SUIS LA TRISTESSE";
+                })
+            }
+        },
+        mounted: function () {
+            this.getMyJoke()
+        }
+    }
+
+</script>
+
+<style>
+    .fond{
+        background-color: white;
+        color: black;
+        background-repeat: no-repeat;
+        text-align: center;
+        
+        
+    }
+    .collapsible {
+    background-color: #777;
+    color: white;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+}
+
+.active, .collapsible:hover {
+    background-color: #555;
+}
+
+.content {
+    padding: 0 18px;
+    display: none;
+    overflow: hidden;
+    background-color: #f1f1f1;
+}
+</style>
